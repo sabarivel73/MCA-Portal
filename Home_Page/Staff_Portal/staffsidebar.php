ef139f179f9bd70e938f9c8bb6956f9097c50571
sabarivel73 <sabarivel0928@gmail.com>
2025-04-25 21:15:40 +0530

    new files

--- Home_Page/Staff_Portal/staffsidebar.php
+++ Home_Page/Staff_Portal/staffsidebar.php
@@ -1,9 +1,9 @@
 <div class="navbar">
  <nav>
    <ul>
-        <button><a href="upload_documents.php">Store Documents</a></button>
-        <button><a href="view_documents.php">View Documents</a></button>
-        <button><a href="mhome.php">Staff Portal</a></button>
+        <button><a href="supload_documents.php">Store Documents</a></button>
+        <button><a href="sview_documents.php">View Documents</a></button>
+        <button><a href="smhome.php">Staff Portal</a></button>
         <button><a href="schangepass.php">Change Password</a></button>
         <button><a href="logout.php">Logout</a></button>
    </ul>
---
+++ Home_Page/Student_Portal/upload_documents.php
@@ -0,0 +1,67 @@
+<?php
+    session_start();
+    include"database.php";
+    function countRecord($sql,$db)
+    {
+        $res=$db->query($sql);
+        return $res->num_rows;
+    }
+    if(!isset($_SESSION["ID"]))
+    {
+	    header("location:ulogin.php");
+    }
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>MCA Portal</title>
+        <style>
+            *
+            {
+                margin:0;
+                padding:0;
+                box-sizing:border-box;
+                font-family:'poppins',sans-serif;
+                cursor:pointer;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+          <div class="center">
+            <?php
+                if(isset($_POST["submit"]))
+                {
+                    $target_dir = "upload/";
+                    $target_file = $target_dir.basename($_FILES["efile"]["name"]);
+                    if(move_uploaded_file($_FILES["efile"]["tmp_name"],$target_file))
+                    {
+                        $sql="insert into udocuments(BTITLE,KEYWORD,FILE,UID) values ('{$_POST["bname"]}','{$_POST["keys"]}','{$target_file}',{$_SESSION["ID"]})";
+                        $db->query($sql);
+                        echo"<p class='success'>Uploaded Successfully</p>";
+                    }
+                    else 
+                    {
+                        echo"<p class='error'>Error in Upload</p>";
+                    }
+                }   
+            ?>
+            <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post" enctype="multipart/form-data">
+                <h3>Upload Documents</h3>
+                <label>Title</label>
+                <input type="text" name="bname" required>
+                <label>Keyword</label>
+                <input type="text" name="keys" required>
+                <label>Upload File</label>
+                <input type="file" name="efile" required>
+                <button type="submit" name="submit">Upload</button>
+                <a href="upload_documents.php">Refresh</a>
+                <a href='uhome.php'>Home</a>
+            </form>      
+          </div>
+        </div>
+        <div class="footer">
+            <p>Copyright &copy; MCA Portal 2025</p>
+        </div>
+    </body>
+</html>
