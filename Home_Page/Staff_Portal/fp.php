9df205cc8e5f4a67990585865352b6192ea28331
Sabari Vel <sabarivel0928@gmail.com>
2025-06-07 17:07:34 +0530

    Update fp.php

--- Home_Page/Staff_Portal/fp.php
+++ Home_Page/Staff_Portal/fp.php
@@ -21,8 +21,8 @@
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'gskgsk3773@gmail.com';
-            $mail->Password = 'fcgq sojw hmyl tzhv';
+            $mail->Username = 'yourmail@gmail.com';
+            $mail->Password = 'mail_password';
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
             $mail->setFrom('mcaportal@gmail.com','MCA Portal');
